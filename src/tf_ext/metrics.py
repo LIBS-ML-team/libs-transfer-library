2660612d1cc461ae08b71ff7dff5fe1a475eddf7
RejectHumanity <99607927+RejectHumanity@users.noreply.github.com>
2022-02-18 14:40:17 +0100

    Update and rename src/metrics.py to src/tf_ext/metrics.py

--- src/tf_ext/metrics.py
+++ src/tf_ext/metrics.py
@@ -15,7 +15,12 @@ def tensor_sparsity(vector):
                    )
   
 
-def make_test_scorer(metric, X_test, y_test):
+def make_val_scorer(metric, X_test, y_test):
   def test_scorer(estimator, *args, **kwargs):
     return metric(y_test, estimator.predict(X_test))
   return test_scorer
+
+def make_baselined_scorer_from_loss(loss, baseline_loss):
+  def score(y_true, y_pred):
+      return 1 - loss(y_true, y_pred) / baseline_loss
+  return score
