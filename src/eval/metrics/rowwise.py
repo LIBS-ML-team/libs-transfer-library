60c35d42bb6a7039c8405de4139e63c00d0f7597
RejectHumanity <99607927+RejectHumanity@users.noreply.github.com>
2022-03-01 14:08:27 +0100

    rowwise metrics
    used with error map

---
+++ src/eval/metrics/rowwise.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def rowwise_cosine(y_true, y_pred):
+  """
+  https://stackoverflow.com/questions/49218285/cosine-similarity-between-matching-rows-in-numpy-ndarrays
+  """
+  return np.einsum('ij,ij->i', y_true, y_pred) / (
+              np.linalg.norm(y_true, axis=1) * np.linalg.norm(y_pred, axis=1)
+    )
+  
+def rowwise_mse(y_true, y_pred):
+  return np.square(np.subtract(y_true, y_pred)).mean(1)
+
+def rowwise_rmse(y_true, y_pred):
+  return np.sqrt(np.square(np.subtract(y_true, y_pred)).mean(1))
