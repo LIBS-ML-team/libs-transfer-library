0c91a5051906f263e30e45029a351669b63af9e2
Pavel Nedelnik <p.nedelnik@email.cz>
2022-08-18 16:07:57 +0200

    initial commit

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,26 +1,14 @@
 import warnings
-from typing import Tuple
-
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
-from scipy.signal import savgol_filter
-from sklearn.utils import resample
-from sklearn.preprocessing import normalize
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.feature_selection import SelectFromModel
-from sklearn.utils.validation import check_array, check_is_fitted
-from sklearn.pipeline import Pipeline
-from scipy.integrate import trapz
-
+from sklearn.utils.validation import check_is_fitted, check_array
 
 class LabelCropp(BaseEstimator, TransformerMixin):
   """
-  TODO add nonnumeric labels
+  TODO 
   """
-
   def __init__(self, label_from=None, label_to=None, labels=None):
-    # TODO default values!!!
     self.label_from = label_from
     self.label_to = label_to
     self.labels = labels
@@ -37,219 +25,215 @@ class LabelCropp(BaseEstimator, TransformerMixin):
     X = check_array(X)
     if X.shape[1] != self.n_features_in_:
       raise ValueError('Fit ({}) and Transform ({}) data does not match shape!'.format(self.n_features_in_, X.shape[1]))
-    if self.labels is None:
-      labels = np.arange(self.n_features_in_)
-    else:
-      labels = self.labels
+    labels = self.labels if self.labels is not None else np.arange(self.n_features_in_)
+    label_from = self.label_from if self.label_from is not None else labels[0]
+    label_to = self.label_to if self.label_to is not None else labels[-1]
       
-    if not np.issubdtype(labels.dtype, np.number):
-      raise ValueError('Only implemented for numeric values')
-    if self.label_from > labels.max() or self.label_to < labels.min():
+    if label_from > labels.max() or label_to < labels.min():
       warnings.warn('Labels out of range! Skipping!')
       return X
 
-    idx_from, idx_to = np.argmax(labels >= self.label_from),  len(labels) - np.argmax((labels <= self.label_to)[::-1]) - 1
+    idx_from, idx_to = np.argmax(labels >= label_from),  len(labels) - np.argmax((labels <= label_to)[::-1]) - 1
     return X[:, idx_from: idx_to]
-    
-    
-    
-class Cover(BaseEstimator, TransformerMixin):
+
+
+
+def estimate_baseline(
+    uc_spectrum,
+    **kwargs
+) -> np.ndarray :
   """
+  Estimates the input spectrum's baseline by performing a sliding minimum
+  operation followed by a sliding Gaussian mean smooth_minima
+
+  Possible kwargs: ww_min, ww_smooth
+
+  RETURN the estimated baseline <baseline>:np.array
   """
 
-  def __init__(self, label_from=None, label_to=None, labels=None):
-    self.label_from = label_from
-    self.label_to = label_to
-    self.labels = labels
+  if 'ww_min' not in kwargs: ww_min = 100
+  else: ww_min = kwargs['ww_min']
+  if 'ww_smooth' not in kwargs: ww_smooth = int(ww_min / 2)
+  else: ww_smooth = kwargs['ww_smooth']
+  if isinstance(uc_spectrum, np.ndarray):
+      spectrum_proc = uc_spectrum.copy()
+  else:
+      spectrum_proc = uc_spectrum.to_numpy()
+ 
+  assert len(spectrum_proc.shape) == 1, 'wrong input dimension'
 
-
-  def fit(self, X, y=None):
-    X = check_array(X)
-    self.n_features_in_ = X.shape[1]
-    return self
-
-
-  def transform(self, X, y=None):
-    check_is_fitted(self)
-    X = check_array(X)
-    if X.shape[1] != self.n_features_in_:
-      raise ValueError('Fit ({}) and Transform ({}) data does not match shape!'.format(self.n_features_in_, X.shape[1]))
-    if self.labels is None:
-      labels = np.arange(self.n_features_in_)
-    else:
-      labels = self.labels
-      
-    if not np.issubdtype(labels.dtype, np.number):
-      raise ValueError('Only implemented for numeric values')
-    if self.label_from > labels.max() or self.label_to < labels.min():
-      warnings.warn('Labels out of range! Skipping!')
-      return X
-
-    idx_from, idx_to = np.argmax(labels >= self.label_from),  len(labels) - np.argmax((labels <= self.label_to)[::-1]) - 1
-    X_new = np.array(X, copy=True)
-    X_new[:, idx_from: idx_to] = 0
-    return X_new
-
-
-
-
-class Cropp(BaseEstimator, TransformerMixin):
-
-  def __init__(self, idx_from=2500, idx_to=33500):
-    self.idx_from = idx_from
-    self.idx_to = idx_to
-
-  def fit(self, X, y=None):
-    X = check_array(X)
-    self.n_features_in_ = X.shape[1]
-    return self
-
-  def transform(self, X, y=None):
-    check_is_fitted(self)
-    X = check_array(X)
-    if X.shape[1] != self.n_features_in_:
-      raise ValueError('Fit ({}) and Transform ({}) data does not match shape!'.format(self.n_features_in_, X.shape[1]))
-    return X[:, self.idx_from: self.idx_to]
-    
-    
-
-class DownSampler(BaseEstimator, TransformerMixin):
-  def __init__(self, samples_to_combine=3, strategy='mean'):
-    self.N = samples_to_combine
-    self.strategy = strategy
-    
-
-  def fit(self, X, y=None):
-    X = check_array(X)
-    self.n_features_in_ = X.shape[1]
-    return self
-    
-
-  def transform(self, X, *args, **kwargs):
-    check_is_fitted(self)
-    X = check_array(X)
-    if X.shape[1] != self.n_features_in_:
-      raise ValueError('Fit ({}) and Transform ({}) data does not match shape!'.format(self.n_features_in_, X.shape[1]))
-    if self.strategy == 'mean':
-      # adds additional value for X % N == 0
-      return np.pad(X, [(0, 0), (0, self.N - X.shape[1] % self.N if X.shape[1] % self.N else 0)], mode='edge').reshape((X.shape[0], X.shape[1] // self.N + int(X.shape[1] % self.N > 0), self.N)).mean(axis=2)
-    else:
-      raise NotImplementedError('Strategy not found!')
-      
-      
-
-class SavGol(BaseEstimator, TransformerMixin):
-  def __init__(self, window_length=5, polyorder=3, deriv=2, mode='constant', delta=3):
-    self.window_length = window_length
-    self.polyorder = polyorder
-    self.deriv = deriv
-    self.mode = mode
-    self.delta = delta
-    
-
-  def fit(self, X, y=None):
-    X = check_array(X)
-    self.n_features_in_ = X.shape[1]
-    return self
-    
-
-  def transform(self, X, y=None):
-    check_is_fitted(self)
-    X = check_array(X)
-    if X.shape[1] != self.n_features_in_:
-      raise ValueError('Fit ({}) and Transform ({}) data does not match shape!'.format(self.n_features_in_, X.shape[1]))
-    f = lambda x: savgol_filter(
-        x,
-        window_length=self.window_length,
-        polyorder=self.polyorder,
-        deriv=self.deriv,
-        mode=self.mode,
-        delta=self.delta
-        )
-    return np.apply_along_axis(f, 1, X)
-    
-    
-
-class Normalizer(BaseEstimator, TransformerMixin):
-  def __init__(self, norm='max'):
-    self.norm = norm
-    
+  # spectrum_proc = spectrum_proc - np.min(spectrum_proc)
   
-  def fit(self, X, y=None):
-    X = check_array(X)
-    self.n_features_in_ = X.shape[1]
-    return self
-    
+  baseline = smooth_minima(
+      rolling_minima(
+          spectrum_proc,
+          window_width=ww_min
+      ),
+      window_width=ww_smooth
+  )
+  assert baseline.shape[0] == uc_spectrum.shape[0], \
+    'Estimate Baseline processing error resulted in unexpected output shape'
 
-  def transform(self, X, y=None):
-    check_is_fitted(self)
-    X = check_array(X)
-    if X.shape[1] != self.n_features_in_:
-      raise ValueError('Fit ({}) and Transform ({}) data does not match shape!'.format(self.n_features_in_, X.shape[1]))
-    return normalize(X, norm=self.norm)
-    
-    
+  return(baseline)
 
-class ERFReducer(BaseEstimator, TransformerMixin):
-  def __init__(self, n_estimators=256, random_state=42):
-    self.n_estimators = n_estimators
-    self.random_state = random_state
-    
+
+def rolling_minima(
+    spectrum_input,
+    **kwargs
+) -> np.ndarray :
+  """
+  RETURN the local minima of <spectrum_input> determined by a moving minimum
+  filter of width <kwargs['window_width']>
+
+  Possible kwargs: window_width
+  """
+
+  if 'window_width' not in kwargs: window_width = 200
+  else: window_width = kwargs['window_width']
+
+  if isinstance(spectrum_input, pd.core.series.Series):
+      spectrum_proc = spectrum_input.copy()
+  else:
+      spectrum_proc = pd.Series(spectrum_input)  
   
-  def fit(self, X, y):
-    X = check_array(X)
-    self.n_features_in_ = X.shape[1]
-    clf = ExtraTreesClassifier(n_estimators = self.n_estimators, n_jobs=-1, random_state=self.random_state)
-    selector = SelectFromModel(clf, prefit=False).fit(X, y)
-    self.filter_ = pd.Series(selector.get_support(), dtype=bool)
-    return self
-    
+  assert len(spectrum_proc.shape) == 1, 'wrong input dimension'
 
-  def transform(self, X, y=None):
-    check_is_fitted(self)
-    X = check_array(X)
-    if X.shape[1] != self.n_features_in_:
-      raise ValueError('Fit ({}) and Transform ({}) data does not match shape!'.format(self.n_features_in_, X.shape[1]))
-    return X[:, self.filter_.values[:X.shape[1]]]
-    
-    
-    
-class Integrate(BaseEstimator, TransformerMixin):
-  def __init__(self, func=trapz, *args, **kwargs):
-    self.func = func
-    self.args = args
-    self.kwargs = kwargs
+  minima_left = spectrum_proc.rolling(
+      window=window_width,
+      min_periods=1
+  ).min()
+  minima_right = spectrum_proc[::-1].rolling(
+      window=window_width,
+      min_periods=1
+  ).min()[::-1]
 
-  def fit(self, X):
-    return self
+  rolling_minima = np.array(
+      [
+          minima_left.to_numpy(), 
+          minima_right.to_numpy()
+      ]
+  ).mean(axis=0)
 
-  def transform(self, X):
-    return pd.DataFrame(self.func(X, *self.args, **self.kwargs))
-    
-    
-    
-def match_wavelengths(original: pd.DataFrame, other: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+  assert rolling_minima.shape[0] == spectrum_input.shape[0], \
+    'Rolling Minima processing error resulted in unexpected output shape'
+
+  return(rolling_minima)
+
+
+def smooth_minima(
+    spectrum_input,
+    **kwargs
+) -> np.ndarray :
   """
-  Match the dimension of each spectra in the <other> dataset to the <original>.
-  Both datasets need to have column names set to the corresponding wavelengths.
-  Finds the intersection, interpolates the <other> dataset to match, fills rest with zeros.
+  RETURN <spectrum_input> smoothed by a sliding Gaussian weighted mean operator 
+
+  Possible kwargs: window_width
   """
-  if len(original.columns) < len(other.columns):
-      print('[WARNING] <original> has smaller dimension than <other>!')
-  print('[WARNING] Experimental!')
-  # find the intersection
-  try:
-    start = np.where(original.columns <= other.columns[0])[0][-1]
-  except:
-    start = 0
-  try:
-    end = np.where(original.columns >= other.columns[-1])[0][0]
-  except:
-    end = -1
-  intersection = original.columns[start:end]
-  # interpolate <other> to match
-  new = other.apply(lambda x: np.interp(intersection, xp=other.columns, fp=x), axis=1, result_type="expand").values
-  # fill with zeros
-  new = pd.DataFrame(np.pad(new, ((0, 0), (start, len(original.columns) - end)), 'constant', constant_values=0).astype(np.float32))
-  new.columns = original.columns
-  return original, new
+
+  if 'window_width' not in kwargs: window_width = 200
+  else: window_width = kwargs['window_width']
+
+  append_size = int(window_width)
+
+  if isinstance(spectrum_input, pd.core.series.Series):
+    spectrum_proc = spectrum_input.copy()
+  else:
+    spectrum_proc = pd.Series(spectrum_input)
+  
+  assert len(spectrum_proc.shape) == 1, \
+    'wrong input dimension in baseline smooth_minima'
+
+  spectrum_proc = pd.Series(
+      np.zeros((append_size))
+  ).append(
+      spectrum_proc
+  ).append(
+      pd.Series(
+        np.zeros((append_size))
+      )
+  )
+
+  smooth_minima_left = spectrum_proc.rolling(
+      window=window_width,
+      min_periods=1,
+      win_type='gaussian'
+  ).mean(std=window_width)
+
+  smooth_minima_right = spectrum_proc.iloc[::-1].rolling(
+      window=window_width,
+      min_periods=1,
+      win_type='gaussian'
+  ).mean(std=window_width)
+
+  smooth_minima_right = smooth_minima_right.iloc[::-1]
+
+  smooth_minima = smooth_minima_left[append_size:-append_size]
+  smooth_minima += smooth_minima_right[append_size:-append_size]
+  smooth_minima /= 2
+
+  assert smooth_minima.shape[0] == spectrum_input.shape[0], \
+    'smooth_minima processing error resulted in unexpected output shape'
+
+  return(smooth_minima)
+
+
+def correct_baseline(
+  spectrum_input,
+  **kwargs
+) -> np.ndarray :
+  """
+  RETURN the baseline corrected spectrum
+
+  Possible kwargs: ww_min, ww_smooth
+  """
+
+  if 'win_min' not in kwargs: kwargs['ww_min'] = 200  
+
+  if 'ww_smooth' not in kwargs: kwargs['ww_smooth'] = kwargs['ww_min'] // 2
+
+  if isinstance(spectrum_input, np.ndarray):
+    spectrum_proc = spectrum_input.copy()
+  else:
+    spectrum_proc = spectrum_input.to_numpy()
+  
+  baseline_correcter_spectrum = spectrum_proc - estimate_baseline(
+      spectrum_proc, 
+      ww_min=kwargs['ww_min'],
+      ww_smooth=kwargs['ww_smooth']
+  )
+  
+  return(baseline_correcter_spectrum)
+
+
+def match_wavelengths(left, right, left_calibration, right_calibration):
+  """
+  Resample two spectral datasets to have common calibration.
+  [left1(left2,right1)left3] or [left1(left2,right1]right2)
+  """
+  if left_calibration[0] > right_calibration[0]:
+    right, left, calibration = match_wavelengths(right, left, right_calibration, left_calibration)
+    return left, right, calibration
+
+  # find the calibration for each part
+  # left from the intersection
+  left1 = left_calibration[left_calibration < right_calibration[0]]
+  # intersection
+  left2 = left_calibration[(left_calibration >= right_calibration[0]) & (left_calibration <= right_calibration[-1])]
+  # right from the intersection
+  left3 = left_calibration[left_calibration > right_calibration[-1]]
+
+  # intersection
+  right1 = right_calibration[(right_calibration >= left_calibration[0]) & (right_calibration <= left_calibration[-1])]
+  # right from the intersection
+  right2 = right_calibration[right_calibration > left_calibration[-1]]
+
+  # combine the calibrations using the finer one for the intersection
+  if np.mean(np.diff(left2)) < np.mean(np.diff(right1)):
+    calibration = np.hstack((left1, left2, right2, left3))
+  else:
+    calibration = np.hstack((left1, right1, right2, left3))
+
+  # resample the spectra
+  left = np.apply_along_axis(lambda x: np.interp(calibration, left_calibration, x, left=0., right=0.), 1, left)
+  right = np.apply_along_axis(lambda x: np.interp(calibration, right_calibration, x, left=0., right=0.), 1, right)
+  return left, right, calibration
--- src/rowwise_metrics.py
+++ src/rowwise_metrics.py
@@ -4,7 +4,7 @@ def rowwise_cosine(y_true, y_pred):
   """
   https://stackoverflow.com/questions/49218285/cosine-similarity-between-matching-rows-in-numpy-ndarrays
   """
-  return 1 - np.einsum('ij,ij->i', y_true, y_pred) / (
+  return - np.einsum('ij,ij->i', y_true, y_pred) / (
               np.linalg.norm(y_true, axis=1) * np.linalg.norm(y_pred, axis=1)
     )
   
@@ -12,4 +12,10 @@ def rowwise_mse(y_true, y_pred):
   return np.square(np.subtract(y_true, y_pred)).mean(1)
 
 def rowwise_rmse(y_true, y_pred):
-  return np.sqrt(np.square(np.subtract(y_true, y_pred)).mean(1))
+  return np.sqrt(rowwise_mse(y_true, y_pred))
+  
+def rowwise_se(y_true, y_pred):
+  return np.sum(np.square(np.subtract(y_true, y_pred)), axis=1)
+  
+def rowwise_euclid(y_true, y_pred):
+  return np.sqrt(rowwise_se(y_true, y_pred))
